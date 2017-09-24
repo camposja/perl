2df5120f07e65e2b11dab4536906c77aae09bd53
Jose A. Campos <jjoseccampos@gmail.com>
2017-09-24 11:15:21 -0400

    initial commit

---
+++ main.pl
@@ -0,0 +1,23 @@
+  use strict;
+  use warnings;
+
+  use LWP::Simple;
+
+  sub main {
+    print "Downloading ... processing your request ...\n";
+    # print get("http://thesum.ca/");
+    # getstore("http://thesum.ca/", "home.html");
+
+    my $picture = getstore('http://thesum.ca/images/logo.jpg', "sum.png");
+
+    if($picture == 200) {
+      print "Success!!\n";
+    }
+    else {
+      print "failed  :( \n";
+    }
+
+    print "Your request is complete!  :) \n";
+  }
+
+  main();
